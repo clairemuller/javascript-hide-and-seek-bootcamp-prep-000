ce00e1cada2b02c18bab3a3efb8eac6b9ce3e682
Claire Muller <clairermuller@gmail.com>
2019-01-15 21:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function deepestChild() {
+  let node = document.querySelector('#grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
+
+
+function increaseRankBy(n) {
+  let rank = document.querySelectorAll('.ranked-list');
+
+}
